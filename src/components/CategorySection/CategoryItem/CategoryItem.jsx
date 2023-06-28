2e82882e759375ab5418b9c174b3d62eb355fd24
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-06-28 19:48:30 +0200

    fix: item page now displays correct info

--- src/components/CategorySection/CategoryItem/CategoryItem.jsx
+++ src/components/CategorySection/CategoryItem/CategoryItem.jsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
+import { GET } from "../../../utils/HTTP";
 import "./index.css";
 
-const CategoryItem = ({ data, setSection }) => {
+const CategoryItem = ({ data, setModal }) => {
   const nameArr = data.name.split(" ");
 
+  const entryName = useRef(data.name);
+
+  const onHandleClick = () => {
+    GET("entry/" + entryName.current).then((data) => {
+      setModal(data.data);
+    });
+  };
+
   return (
-    <div className="CategoryItem" onClick={() => setSection("item")}>
+    <div className="CategoryItem" onClick={onHandleClick}>
       <img src={data.image} alt={data.name} />
       <p className="CategoryItem__Name">
         {nameArr
